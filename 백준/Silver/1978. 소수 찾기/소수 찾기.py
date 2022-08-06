1f5fbedadd5c4dc80250ce3c23c4788f284f011e
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-08-06 21:26:10 +0900

    [Silver V] Title: 소수 찾기, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.py
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -1,17 +1,14 @@
 import sys
 
 def f(lst):
-    numbers = list(range(2, 1001))
-    result = 0
-    for i in range(3, 1001):
+    result = N
+    for i in lst:
         for j in range(2, i):
             if i % j == 0:
-                if i in numbers:
-                    numbers.remove(i)
-                    break
-    for i in lst:
-        if i in numbers:
-            result += 1
+                result -= 1
+                break
+        if i == 1:
+            result -= 1
     return result
 
 N = int(input())
