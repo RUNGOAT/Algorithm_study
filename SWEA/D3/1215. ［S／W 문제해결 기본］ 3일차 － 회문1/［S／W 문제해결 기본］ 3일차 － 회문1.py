3ca800ada6d19927f5dd4a3ddb2d647d33968e45
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-08-09 23:32:19 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문1, Time: 154 ms, Memory: 58,524 KB -BaekjoonHub

--- SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
+++ SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
@@ -2,12 +2,12 @@ for t in range(1, 11):
     p = int(input())    # palindrome
     board = []
     for i in range(8):
-        board.append(list(map(str, input())))
+        board.append(list(input()))
     cnt = 0
 
     for i in range(8):          # 가로
         for j in range(8-p+1):
-            if board[i][j:j+p] == list(reversed(board[i][j:j+p])):
+            if board[i][j:j+p] == board[i][j:j+p][::-1]:
                 cnt += 1
 
     for j in range(8):          # 세로
@@ -15,7 +15,7 @@ for t in range(1, 11):
             temp = []
             for x in range(p):
                 temp.append(board[i+x][j])
-            if temp == list(reversed(temp)):
+            if temp == temp[::-1]:
                 cnt += 1
 
     print(f'#{t} {cnt}')
