e8835ab6833bb2d91ced69c201b3a755f56a7877
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-08-09 17:51:17 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 161 ms, Memory: 61,680 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
@@ -2,16 +2,16 @@ for _ in range(10):
     t = int(input())
     nums = list(map(int, input().split()))
     cnt = 0
-    while True:
+    play = True
+    while play:
         for i in range(1, 6):
-            if nums[i-1] <= i:
+            if nums[i - 1] <= i:
                 nums.append(0)
                 secret = nums[i:]
+                play = False
                 break
-            nums.append(nums[i-1] - i)
+            nums.append(nums[i - 1] - i)
         nums = nums[i:]
-        if nums[-1] == 0:
-            break
-            
+
     print(f'#{t}', end=' ')
     print(*secret)
