d81d15ce9a3930b20126256781da3a716836ed8b
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-08-12 01:13:36 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 239 ms, Memory: 63,572 KB -BaekjoonHub

---
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.py
@@ -0,0 +1,31 @@
+for _ in range(1, 11):
+    tc = int(input())
+    N = 100
+    arr = [list(map(int, input().split())) for _ in range(N)]
+ 
+    di = [1, 0, 0]  # 아래 왼쪽 오른쪽
+    dj = [0, -1, 1]
+    for j in range(N):
+        if arr[0][j] == 1:
+            result = j
+            i = 0
+            d = 0
+            ni = 0
+            while ni < N-1:
+                ni = i + di[d]
+                nj = j + dj[d]
+                if 0 <= ni < N and 0 <= nj < N and arr[ni][nj] == 1:
+                    i = ni
+                    j = nj
+                    if j + dj[1] != -1 and arr[i+di[1]][j+dj[1]] == 1 and d == 0:    # 왼쪽
+                        d = 1
+                    elif j+dj[2] != N and arr[i+di[2]][j+dj[2]] == 1 and d == 0:    # 오른쪽
+                        d = 2
+                else:
+                    d = 0    # 아래
+            else:
+                i = 99
+ 
+            if arr[i][j] == 2:
+                print(f'#{tc} {result}')
+                break
