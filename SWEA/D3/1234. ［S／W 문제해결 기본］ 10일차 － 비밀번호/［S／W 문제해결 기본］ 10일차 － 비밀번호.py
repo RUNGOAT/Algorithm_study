5a98590fb39d245a8c6d87cd83eed4389f16d75b
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-08-11 22:57:41 +0900

    [D3] Title: [S/W 문제해결 기본] 10일차 - 비밀번호, Time: 156 ms, Memory: 58,524 KB -BaekjoonHub

--- SWEA/D3/1234. ［S／W 문제해결 기본］ 10일차 － 비밀번호/［S／W 문제해결 기본］ 10일차 － 비밀번호.py
+++ SWEA/D3/1234. ［S／W 문제해결 기본］ 10일차 － 비밀번호/［S／W 문제해결 기본］ 10일차 － 비밀번호.py
@@ -1,22 +1,24 @@
+def new_list(lst, x, y):
+    temp = []
+    for i in range(len(lst)):
+        if i != x and i != y:
+            temp.append(lst[i])
+    return temp
+
+
 for tc in range(1, 11):
     N, nums = input().split()
     N = int(N)
     secret = list(map(int, nums))
 
-    for x in range(N // 2):
-        N = len(secret)
-        temp = []
-        i = 0
-        while i < N-1:
-            if secret[i] != secret[i+1]:
-                temp.append(secret[i])
-            else:
-                i += 1
-            i += 1
-            if i == N - 1:
-                temp.append(secret[i])
-        secret = temp
+    play = True
+    while play:
+        for i in range(len(secret)-1):
+            if secret[i] == secret[i+1]:
+                secret = new_list(secret, i, i+1)
+                break
+        else:
+            play = False
 
     print(f'#{tc}', end=' ')
     print(*secret, sep='')
-    
