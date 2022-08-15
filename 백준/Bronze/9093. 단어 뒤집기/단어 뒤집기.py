f0df329d46f8a6126137b30b7f19cadea7000c80
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-08-15 17:57:05 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 128 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
@@ -1,6 +1,8 @@
+import sys
+
 T = int(input())
 for _ in range(T):
-    sentence = list(input().split())
+    sentence = list(sys.stdin.readline().split())
     for i in range(len(sentence)):
         sentence[i] = sentence[i][::-1]
     print(' '.join(sentence))
