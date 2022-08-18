3eb8032a71b2a15a49a49f88d5423a0a22d0a782
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-08-18 22:17:51 +0900

    [Bronze II] Title: 방 배정, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.py
+++ 백준/Bronze/13300. 방 배정/방 배정.py
@@ -2,37 +2,22 @@ import sys
 
 N, K = map(int, sys.stdin.readline().split())
 
-male = {}
-female = {}
-for _ in range(N):
-    s, y = map(int, sys.stdin.readline().split())
-
-    if s == 1:
-        if y in male:
-            male[y] += 1
-        else:
-            male[y] = 1
+student_dict = {}
+for i in range(N):
+    student = tuple(sys.stdin.readline().split())
+    if student in student_dict:
+        student_dict[student] += 1
     else:
-        if y in female:
-            female[y] += 1
-        else:
-            female[y] = 1
+        student_dict[student] = 1
 
 ans = 0
-for value in male.values():
+for value in student_dict.values():
     if value > K:
-       ans += value // K
-       if value % K:
-           ans += 1
-    else:
-        ans += 1
-
-for value in female.values():
-    if value > K:
-       ans += value // K
-       if value % K:
-           ans += 1
+        ans += (value // K)
+        if value % K:
+            ans += 1
     else:
         ans += 1
 
 print(ans)
+
