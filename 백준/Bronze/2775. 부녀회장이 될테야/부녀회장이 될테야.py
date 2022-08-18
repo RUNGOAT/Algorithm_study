1ea4f862260d3e67458f0e15b49482efbfb1e461
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-08-18 23:45:04 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -1,11 +1,11 @@
 import sys
 
-T = int(sys.stdin.readline().strip())
-
-for _ in range(T):
+N = int(sys.stdin.readline())
+for _ in range(N):
     k = int(sys.stdin.readline())
     n = int(sys.stdin.readline())
     people = list(range(1, n+1))
+
     for _ in range(k):
         for i in range(1, n):
             people[i] = people[i-1] + people[i]
