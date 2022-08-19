fdf9578a7d3a88aa311435a5fafc7d953ead8b10
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-08-19 16:16:37 +0900

    [Bronze I] Title: 색종이, Time: 1188 ms, Memory: 123172 KB -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -2,14 +2,18 @@ import sys
 
 N = int(sys.stdin.readline())
 arr = [[0] * 1001 for _ in range(1001)]
+ans = [0] * (N+1)
 for n in range(1, N+1):
     s, e, w, h = map(int, sys.stdin.readline().split())
     for i in range(h):
         for j in range(w):
-            arr[e+i][s+j] = n
+            if arr[e+i][s+j] != 0:
+                ans[arr[e+i][s+j]] -= 1
+                arr[e+i][s+j] = n
+                ans[arr[e+i][s+j]] += 1
+            else:
+                arr[e + i][s + j] = n
+                ans[arr[e + i][s + j]] += 1
 
-for x in range(1, N+1):
-    cnt = 0
-    for y in range(1001):
-        cnt += arr[y].count(x)
-    print(cnt)
+for i in range(1, N+1):
+    print(ans[i])
