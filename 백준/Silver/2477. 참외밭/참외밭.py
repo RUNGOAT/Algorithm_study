eb8dc0df98fd2dc2535dd2f8b6cdd417985fa0fe
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-08-20 19:44:37 +0900

    [Silver III] Title: 참외밭, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2477. 참외밭/참외밭.py
+++ 백준/Silver/2477. 참외밭/참외밭.py
@@ -1,10 +1,16 @@
 import sys
 
 
-def empty_area_check():
-    for i in range(4):
-        for j in range(5):
-            if [empty_area[i], empty_area[i + 1]] == [direction[j], direction[j + 1]]:
+def empty_area():
+    '''
+    문제에서 반시계 방향 고정
+    empty_area_sequence : 빈 면적에 해당하는 방향값 순서를 담은 배열
+    연속된 두 개의 숫자가 입력 배열(direction)에 있을 때의 길이가 빈 면적의 가로, 세로 길이
+    '''
+    empty_area_sequence = [1, 3, 2, 4, 1]
+    for i in range(4):          # empty_area_sequence 배열 길이 - 1
+        for j in range(5):      # direction 배열 길이 - 1
+            if [empty_area_sequence[i], empty_area_sequence[i + 1]] == [direction[j], direction[j + 1]]:
                 return length[j] * length[j + 1]
     return length[0] * length[-1]
 
@@ -24,6 +30,5 @@ for _ in range(6):
         height.append(l)
 
 area = max(width) * max(height)
-empty_area = [1, 3, 2, 4, 1]
 
-print(K * (area - empty_area_check()))
+print(K * (area - empty_area()))
