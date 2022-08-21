6df153d2fb2258d88c8bef03a724583a842ac0e9
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-08-21 18:06:48 +0900

    [Silver IV] Title: 개미, Time: 112 ms, Memory: 35552 KB -BaekjoonHub

--- 백준/Silver/10158. 개미/개미.py
+++ 백준/Silver/10158. 개미/개미.py
@@ -4,10 +4,13 @@ w, h = map(int, sys.stdin.readline().split())
 p, q = map(int, sys.stdin.readline().split())
 t = int(sys.stdin.readline())
 
+w_idx = t % (w * 2)
+h_idx = t % (h * 2)
+
 i = p
 w_list = [p]
 d = 1
-for _ in range(1, w * 2):
+for _ in range(1, w_idx+1):
     i += d
     w_list.append(i)
     if i + d == w + 1:
@@ -18,7 +21,7 @@ for _ in range(1, w * 2):
 j = q
 h_list = [q]
 d = 1
-for _ in range(1, h * 2):
+for _ in range(1, h_idx+1):
     j += d
     h_list.append(j)
     if j + d == h + 1:
@@ -26,4 +29,4 @@ for _ in range(1, h * 2):
     elif j + d == -1:
         d = 1
 
-print(w_list[t % (w * 2)], h_list[t % (h * 2)])
+print(w_list[-1], h_list[-1])
