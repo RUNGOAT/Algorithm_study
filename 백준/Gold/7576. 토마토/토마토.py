3b3ff3aaa3fe3794211aba4ef193db49717f25a8
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-09-01 21:57:04 +0900

    [Gold V] Title: 토마토, Time: 2156 ms, Memory: 97972 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -0,0 +1,39 @@
+import sys
+from collections import deque
+
+
+def bfs(N, M):
+    q = deque()
+    flag = True
+    for i in range(N):
+        for j in range(M):
+            if arr[i][j] == 1:
+                q.append((i, j))
+            elif arr[i][j] == 0:
+                flag = False
+
+    if flag:
+        return 0
+
+    while q:
+        i, j = q.popleft()
+        for di, dj in [[0, 1], [1, 0], [0, -1], [-1, 0]]:
+            ni, nj = i + di, j + dj
+            if 0 <= ni < N and 0 <= nj < M and arr[ni][nj] == 0:
+                q.append((ni, nj))
+                arr[ni][nj] = arr[i][j] + 1
+
+    ans = -1
+    for i in range(N):
+        for j in range(M):
+            if arr[i][j] == 0:
+                return -1
+            if ans < arr[i][j]:
+                ans = arr[i][j]
+    return ans - 1
+
+
+M, N = map(int, sys.stdin.readline().split())
+arr = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+print(bfs(N, M))
