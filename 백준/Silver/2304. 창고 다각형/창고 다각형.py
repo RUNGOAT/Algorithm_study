7747c03ea9b501f694d2f49b448f00f43180d72f
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-09-03 22:45:00 +0900

    [Silver II] Title: 창고 다각형, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.py
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.py
@@ -1,34 +1,30 @@
 import sys
 
 N = int(sys.stdin.readline())
-pillar = []
+
+storage = [0] * 1001
+max_l = 0
 for _ in range(N):
     l, h = map(int, sys.stdin.readline().split())
-    pillar += [[l] + [h]]
-pillar.sort()
-ans = 0
+    if max_l < l:
+        max_l = l
+    storage[l] = h
 
-stack = [pillar[0]]
-for i in range(1, N):
-    if stack[-1][1] <= pillar[i][1]:
-        stack.append(pillar[i])
+area = 0
+max_h_idx = storage.index(max(storage))
 
-max_h_idx = pillar.index(stack[-1])
-ans += pillar[max_h_idx][1]
+height = 0
+for i in range(max_h_idx):
+    if height < storage[i]:
+        height = storage[i]
+    area += height
 
-temp = []
-if max_h_idx != N-1:
-    temp.append(pillar[N-1])
-for i in range(N-2, max_h_idx, -1):
-    if temp[-1][1] <= pillar[i][1]:
-        temp.append(pillar[i])
+area += storage[max_h_idx]
 
-stack += reversed(temp)
-stack.append([0, 0])
-for i in range(1, len(stack)):
-    if stack[i-1][1] <= stack[i][1]:
-        ans += abs(stack[i][0] - stack[i-1][0]) * stack[i-1][1]
-    else:
-        ans += abs(stack[i][0] - stack[i-1][0]) * stack[i][1]
+height = 0
+for i in range(max_l, max_h_idx, -1):
+    if height < storage[i]:
+        height = storage[i]
+    area += height
 
-print(ans)
+print(area)
