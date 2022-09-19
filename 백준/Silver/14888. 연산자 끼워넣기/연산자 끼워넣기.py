5bea49089b5e46f9085274f53d52f1a0add9d349
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-09-19 21:49:59 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 96 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,39 +1,31 @@
 import sys
-from itertools import permutations
-
 read = sys.stdin.readline
+
+
+def dfs(result, i, plus, minus, multi, div):
+    global max_ans, min_ans
+    if i == N:
+        max_ans = max(result, max_ans)
+        min_ans = min(result, min_ans)
+    else:
+        if plus:
+            dfs(result + num[i], i + 1, plus - 1, minus, multi, div)
+        if minus:
+            dfs(result - num[i], i + 1, plus, minus - 1, multi, div)
+        if multi:
+            dfs(result * num[i], i + 1, plus, minus, multi - 1, div)
+        if div:
+            dfs(int(result / num[i]), i + 1, plus, minus, multi, div - 1)
+
 
 N = int(read())
 num = list(map(int, read().split()))
 opers_tmp = list(map(int, read().split()))
-opers = ['+', '-', '*', '/']
-opers_list = []
-for i in range(4):
-    for j in range(opers_tmp[i]):
-        opers_list.append(opers[i])
 
 max_ans = -9999999999
 min_ans = 9999999999
-for oper_list in permutations(opers_list, N - 1):
-    i = 1
-    result = num[0]
-    for oper in oper_list:
-        if oper == '+':
-            result += num[i]
-        elif oper == '-':
-            result -= num[i]
-        elif oper == '*':
-            result *= num[i]
-        else:
-            if result < 0:
-                result *= -1
-                result //= num[i]
-                result *= -1
-            else:
-                result //= num[i]
-        i += 1
-    max_ans = max(result, max_ans)
-    min_ans = min(result, min_ans)
+
+dfs(num[0], 1, opers_tmp[0], opers_tmp[1], opers_tmp[2], opers_tmp[3])
 
 print(max_ans)
 print(min_ans)
