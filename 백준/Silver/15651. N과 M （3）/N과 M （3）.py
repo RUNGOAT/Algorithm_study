b196d2a09ad1ce1e668626d539f7e5827b49a598
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-09-20 23:03:46 +0900

    [Silver III] Title: N과 M (3), Time: 2060 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,13 @@
+def f(cnt):
+    if cnt == M:
+        print(*tmp)
+    else:
+        for i in range(1, N+1):
+            tmp.append(i)
+            f(cnt + 1)
+            tmp.pop()
+
+
+N, M = map(int, input().split())
+tmp = []
+f(0)
