97981bd7bbcb565935334c325631759c85575284
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-09-21 17:32:13 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 233 ms, Memory: 63,832 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
@@ -1,29 +1,20 @@
-def dfs(index, cnt):
-    global result
-    if cnt == count:
-        result = max(int(''.join(reward)), result)
-        return
-
-    for i in range(index, N):
-        for j in range(i+1, N):
-            if reward[i] <= reward[j]:
+def f(idx, cnt):
+    global max_reward
+    if cnt == int(K):
+        max_reward = max(max_reward, int(''.join(reward)))
+    else:
+        for i in range(idx, N-1):
+            for j in range(i+1, N):
                 reward[i], reward[j] = reward[j], reward[i]
-                dfs(i, cnt+1)
-                reward[j], reward[i] = reward[i], reward[j]
-
-    if result == 0 and cnt < count:
-        if (count - cnt) % 2:
-            reward[-1], reward[-2] = reward[-2], reward[-1]
-        dfs(index, count)
+                f(i, cnt + 1)
+                reward[i], reward[j] = reward[j], reward[i]
 
 
 T = int(input())
 for tc in range(1, T+1):
-    reward, count = input().split()
-    reward = list(reward)
+    reward, K = input().split()
     N = len(reward)
-    count = int(count)
-    result = 0
-    dfs(0, 0)
-
-    print(f'#{tc} {result}')
+    reward = list(reward)
+    max_reward = 0
+    f(0, 0)
+    print(f'#{tc} {max_reward}')
