1af0a328518cabff160321fc53470722552fcb17
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-10-10 19:37:14 +0900

    [Gold III] Title: 아기 상어, Time: 244 ms, Memory: 122952 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -6,7 +6,7 @@ input = sys.stdin.readline
 def bfs(sx, sy):
     global ans, baby, eat
     q = deque([[sx, sy]])
-    feed = []
+    feed = []   # 먹이의 위칫값
     visited = [[0] * N for _ in range(N)]
     visited[sx][sy] = 1
     while q:
@@ -21,16 +21,17 @@ def bfs(sx, sy):
                     feed.append([nx, ny, visited[x][y]])
 
     if feed:
+        # 거리, 위, 왼쪽 순으로 정렬
         feed.sort(key=lambda x: (x[2], x[0], x[1]))
         x, y = feed[0][0], feed[0][1]
-        arr[x][y] = 0
-        ans += feed[0][2]
+        ans += feed[0][2]   # 시간 (== 거리)
+        arr[x][y] = 0   # eat
         eat += 1
         if eat == baby:
             baby += 1
             eat = 0
         bfs(x, y)
-    else:
+    else:       # 먹을 수 있는 것이 없으면
         print(ans)
         exit(0)
 
@@ -44,5 +45,5 @@ ans = eat = 0
 for i in range(N):
     for j in range(N):
         if arr[i][j] == 9:
-            arr[i][j] = 0
+            arr[i][j] = 0   # 9는 아기 상어의 위치를 나타낸 것으로 이동하면 0으로 처리
             bfs(i, j)
