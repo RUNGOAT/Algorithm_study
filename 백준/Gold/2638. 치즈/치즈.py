9190af7988731e4923a05f8c24ba8f6b7383e722
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-10-15 19:08:08 +0900

    [Gold III] Title: 치즈, Time: 100 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -1,47 +1,36 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
 
 
-def bfs(x, y):
-    q = deque([[x, y]])
-    visited[x][y] = 0
-    while q:
-        x, y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < N and 0 <= ny < M and arr[nx][ny] == 0 and visited[nx][ny] == 1:
-                q.append([nx, ny])
-                visited[nx][ny] = 0
-
-
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
+di = [0, 1, 0, -1]
+dj = [1, 0, -1, 0]
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
-
+visited = [[0] * M for _ in range(N)]
+visited[0][0] = -1
+stack = [[0, 0]]
 ans = 0
 while True:
-    visited = [[1] * M for _ in range(N)]
-    bfs(0, 0)
     tmp = []
-    for i in range(N):
-        for j in range(M):
-            cnt = 0
-            if visited[i][j]:
-                for k in range(4):
-                    ni = i + dx[k]
-                    nj = j + dy[k]
-                    if visited[ni][nj] == 0:
-                        cnt += 1
-                if cnt > 1:
-                    tmp.append([i, j])
-
+    while stack:
+        i, j = stack.pop()
+        for x in range(4):
+            ni = i + di[x]
+            nj = j + dj[x]
+            if 0 <= ni < N and 0 <= nj < M:
+                if arr[ni][nj] == 0 and visited[ni][nj] == 0:
+                    visited[ni][nj] = -1
+                    stack.append([ni, nj])
+                elif arr[ni][nj]:
+                    visited[ni][nj] += 1
+                    if visited[ni][nj] > 1:
+                        tmp.append([ni, nj])
+                        visited[ni][nj] = -1
     if tmp:
+        ans += 1
         for i, j in tmp:
             arr[i][j] = 0
-        ans += 1
+        stack = tmp
     else:
         print(ans)
         exit(0)
