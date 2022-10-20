5a00d673bc0138d856ccbd3a02b9e6d73134772f
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-10-21 02:08:37 +0900

    [Gold IV] Title: 테트로미노, Time: 192 ms, Memory: 119480 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -4,46 +4,34 @@ input = sys.stdin.readline
 
 def dfs(x, y, cnt, ssum):
     global ans
+    if ssum + max_v * (4 - cnt) <= ans:
+        return
     if cnt == 4:
         ans = max(ans, ssum)
         return
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        if 0 <= nx < N and 0 <= ny < M:
-            tmp = arr[nx][ny]
-            arr[nx][ny] = 0
-            dfs(nx, ny, cnt + 1, ssum + tmp)
-            arr[nx][ny] = tmp
-
-
-def ㅗ(x, y, cnt, ssum):
-    min_v = 1000
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < N and 0 <= ny < M:
-            cnt += 1
-            ssum += arr[nx][ny]
-            min_v = min(min_v, arr[nx][ny])
-    if cnt == 5:
-        ssum -= min_v
-    elif cnt < 4:
-        return 0
-    return ssum
+        if 0 <= nx < N and 0 <= ny < M and visited[nx][ny] == 0:
+            if cnt == 2:
+                visited[nx][ny] = 1
+                dfs(x, y, cnt + 1, ssum + arr[nx][ny])
+                visited[nx][ny] = 0
+            visited[nx][ny] = 1
+            dfs(nx, ny, cnt + 1, ssum + arr[nx][ny])
+            visited[nx][ny] = 0
 
 
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
+visited = [[0] * M for _ in range(N)]
+max_v = max(map(max, arr))
 ans = 0
 for i in range(N):
     for j in range(M):
-        tmp = arr[i][j]
-        arr[i][j] = 0
-        dfs(i, j, 1, tmp)
-        arr[i][j] = tmp
-        ans = max(ans, ㅗ(i, j, 1, tmp))
-
+        visited[i][j] = 1
+        dfs(i, j, 1, arr[i][j])
+        visited[i][j] = 0
 print(ans)
