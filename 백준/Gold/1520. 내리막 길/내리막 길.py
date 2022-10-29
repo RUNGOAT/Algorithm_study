23ab72298625999ef2d0b89acfa3170aae939b91
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-10-29 18:06:56 +0900

    [Gold III] Title: 내리막 길, Time: 156 ms, Memory: 42548 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -1,26 +1,25 @@
-import heapq
 import sys
+sys.setrecursionlimit(10 ** 8)
 input = sys.stdin.readline
 
 
-def bfs2(x, y, v):
-    q = [(v, x, y)]
-    visited = [[0] * N for _ in range(M)]
-    visited[x][y] = 1
-    while q:
-        v, x, y = heapq.heappop(q)
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < M and 0 <= ny < N and v < arr[nx][ny]:
-                if not visited[nx][ny]:
-                    heapq.heappush(q, (arr[nx][ny], nx, ny))
-                visited[nx][ny] += visited[x][y]
-    return visited[0][0]
+def dfs(x, y):
+    if dp[x][y] != -1:
+        return dp[x][y]
+    cnt = 0
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < M and 0 <= ny < N and arr[nx][ny] < arr[x][y]:
+            cnt += dfs(nx, ny)
+    dp[x][y] = cnt
+    return dp[x][y]
 
 
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 M, N = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(M)]
-print(bfs2(M-1, N-1, arr[M-1][N-1]))
+dp = [[-1] * N for _ in range(M)]
+dp[M - 1][N - 1] = 1
+print(dfs(0, 0))
