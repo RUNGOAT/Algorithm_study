a131aa3b7959dda229fc4ab27c43c15e5d319302
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-11-05 21:30:30 +0900

    [Silver I] Title: 신입 사원, Time: 6680 ms, Memory: 71072 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    arr = sorted([list(map(int, input().split())) for _ in range(N)])
+    ans = 1
+    min_v = arr[0][1]
+    for i in range(1, N):
+        if arr[i][1] < min_v:
+            ans += 1
+            min_v = arr[i][1]
+    print(ans)
