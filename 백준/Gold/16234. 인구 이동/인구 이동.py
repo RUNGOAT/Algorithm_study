c5fbcb7f493d26a898f6732f74a158e79a007130
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-11-10 22:32:57 +0900

    [Gold V] Title: 인구 이동, Time: 1300 ms, Memory: 164348 KB -BaekjoonHub

---
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -0,0 +1,45 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**4)
+
+def dfs(x, y):
+    global flag, cnt, po_sum
+    visited[x][y] = union_idx
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        if 0 <= nx < N and 0 <= ny < N and visited[nx][ny] == 0 and L <= abs(arr[nx][ny] - arr[x][y]) <= R:
+            cnt += 1
+            po_sum += arr[nx][ny]
+            dfs(nx, ny)
+            flag = False
+
+
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+N, L, R = map(int, input().split())
+arr = []
+for _ in range(N):
+    arr.append(list(map(int, input().split())))
+
+date = 0
+visited = [[0] * N for _ in range(N)]
+while True:
+    union_po = {}
+    union_idx = 1
+    flag = True
+    for i in range(N):
+        for j in range(N):
+            if not visited[i][j]:
+                po_sum = arr[i][j]
+                cnt = 1
+                dfs(i, j)
+                union_po[union_idx] = po_sum // cnt
+                union_idx += 1
+    if flag:
+        print(date)
+        break
+    for i in range(N):
+        for j in range(N):
+            arr[i][j] = union_po.get(visited[i][j], arr[i][j])
+            visited[i][j] = 0
+    date += 1
