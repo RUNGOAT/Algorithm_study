ad2d30668693092dd17be5c52b3c25239db767d2
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-11-12 23:14:48 +0900

    [Gold V] Title: LCS, Time: 620 ms, Memory: 54008 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -5,11 +5,9 @@ arr1 = [''] + list(input().strip())
 arr2 = [''] + list(input().strip())
 LCS = [[0] * (len(arr2)) for _ in range(len(arr1))]
 
-for i in range(len(arr1)):
-    for j in range(len(arr2)):
-        if i == 0 or j == 0:
-            LCS[i][j] = 0
-        elif arr1[i] == arr2[j]:
+for i in range(1, len(arr1)):
+    for j in range(1, len(arr2)):
+        if arr1[i] == arr2[j]:
             LCS[i][j] = LCS[i-1][j-1] + 1
         else:
             LCS[i][j] = max(LCS[i-1][j], LCS[i][j-1])
