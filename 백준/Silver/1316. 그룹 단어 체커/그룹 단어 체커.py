45b09d0079fe5289a9fe9de947ac1a2d90f2f158
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-11-19 00:04:08 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,14 +1,16 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
 ans = 0
 for _ in range(N):
-    word = input()
-    temp = [word[0]]
+    word = input().strip()
+    tmp = word[0]
+    cnt = 1
     for i in range(1, len(word)):
-        if word[i] not in temp or word[i] == temp[i-1]:
-            temp.append(word[i])
-        else:
-            break
-    else:
+        if word[i] != tmp:
+            cnt += 1
+            tmp = word[i]
+    if cnt == len(set(word)):
         ans += 1
-
 print(ans)
