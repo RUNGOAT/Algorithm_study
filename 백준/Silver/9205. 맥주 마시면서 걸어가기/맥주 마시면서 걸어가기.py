227b7fc21748bc25e79c83d10f5759c768e159e7
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-11-26 23:49:19 +0900

    [Silver I] Title: 맥주 마시면서 걸어가기, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
+++ 백준/Silver/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
@@ -1,20 +1,19 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
 
 
 def bfs(x, y):
-    q = deque()
-    q.append([x, y])
-    while q:
-        sx, sy = q.popleft()
+    visited = [False] * N
+    stack = [[x, y]]
+    while stack:
+        sx, sy = stack.pop()
         if abs(ex - sx) + abs(ey - sy) <= 1000:
             return 'happy'
         for i in range(N):
             if not visited[i]:
                 x, y = market[i]
                 if abs(x - sx) + abs(y - sy) <= 1000:
-                    q.append([x, y])
+                    stack.append([x, y])
                     visited[i] = True
     return 'sad'
 
@@ -28,5 +27,4 @@ for _ in range(T):
         market.append(list(map(int, input().split())))
     market.sort(key=lambda x: (x[0], x[1]))
     ex, ey = map(int, input().split())
-    visited = [False] * N
     print(bfs(sx, sy))
