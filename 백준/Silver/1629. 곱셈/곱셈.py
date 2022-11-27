70b7d0209c0c77c065cb4255fab44dc1d422e8e3
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-11-27 21:39:39 +0900

    [Silver I] Title: 곱셈, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,13 +1,2 @@
-def power(x, n):
-    if n == 0:
-        return 1
-
-    y = power(x, n // 2) % C
-
-    if n % 2 == 0:
-        return y * y
-    else:
-        return y * y * x
-
 A, B, C = map(int, input().split())
-print(power(A, B) % C)
+print(pow(A, B, C))
