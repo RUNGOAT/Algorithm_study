3d3f139ef3c27aa909e17aa1ecd130393ad17554
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-12-06 23:23:44 +0900

    [Gold IV] Title: 플로이드, Time: 228 ms, Memory: 115948 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,35 +1,30 @@
-import heapq
 import sys
 input = sys.stdin.readline
 
-
-def dijkstra(v):
-    heap = []
-    heapq.heappush(heap, (0, v))
-    D[v] = 0
-    while heap:
-        d, v = heapq.heappop(heap)
-        for w in range(1, N+1):
-            cost = d + adjM[v][w]
-            if D[w] > cost:
-                D[w] = cost
-                heapq.heappush(heap, (cost, w))
-
-
-INF = 100000000
+INF = int(1e9)
 N = int(input())
 M = int(input())
-adjM = [[INF] * (N+1) for _ in range(N+1)]
+
+graph = [[INF] * (N + 1) for _ in range(N + 1)]
+
+for a in range(1, N + 1):
+    for b in range(1, N + 1):
+        if a == b:
+            graph[a][b] = 0
+
 for _ in range(M):
     a, b, c = map(int, input().split())
-    adjM[a][b] = min(adjM[a][b], c)
+    graph[a][b] = min(c, graph[a][b])
 
-for n in range(1, N+1):
-    D = [INF] * (N+1)
-    dijkstra(n)
-    for i in range(1, N+1):
-        if D[i] == INF:
+for k in range(1, N + 1):
+    for a in range(1, N + 1):
+        for b in range(1, N + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+for a in range(1, N + 1):
+    for b in range(1, N + 1):
+        if graph[a][b] == INF:
             print(0, end=' ')
         else:
-            print(D[i], end=' ')
+            print(graph[a][b], end=' ')
     print()
