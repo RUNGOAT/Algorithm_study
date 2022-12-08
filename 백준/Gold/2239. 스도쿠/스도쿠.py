4dee5292ffcd1a042ef1ff53c9d698b6ed960682
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-12-08 21:37:22 +0900

    [Gold IV] Title: 스도쿠, Time: 4104 ms, Memory: 150552 KB -BaekjoonHub

---
+++ 백준/Gold/2239. 스도쿠/스도쿠.py
@@ -0,0 +1,42 @@
+import sys
+def check(x, y, k):
+    for j in range(9):
+        if k == arr[x][j]:
+            return False
+
+    for i in range(9):
+        if k == arr[i][y]:
+            return False
+
+    h = 3 * (x // 3)
+    w = 3 * (y // 3)
+    for i in range(h, h+3):
+        for j in range(w, w+3):
+            if k == arr[i][j]:
+                return False
+    return True
+
+
+def sudoku(idx):
+    if idx == zero_cnt:
+        for i in range(9):
+            print(''.join(map(str, arr[i])))
+        exit(0)
+
+    x, y = zero_posi[idx]
+    for k in range(1, 10):
+        if check(x, y, k):
+            arr[x][y] = k
+            sudoku(idx+1)
+            arr[x][y] = 0
+
+
+arr = [list(map(int, sys.stdin.readline().strip())) for _ in range(9)]
+zero_posi = []
+zero_cnt = 0
+for i in range(9):
+    for j in range(9):
+        if arr[i][j] == 0:
+            zero_posi.append((i, j))
+            zero_cnt += 1
+sudoku(0)
