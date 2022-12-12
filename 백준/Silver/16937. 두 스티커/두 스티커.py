20dbd5cd91a346d0525a64c074ff8f16165172e0
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-12-12 23:51:15 +0900

    [Silver III] Title: 두 스티커, Time: 64 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/16937. 두 스티커/두 스티커.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+
+H, W = map(int, input().split())
+if H < W:
+    H, W = W, H
+N = int(input())
+arr = []
+for _ in range(N):
+    r, c = map(int, input().split())
+    arr.append((r, c))
+max_area = 0
+for i in range(N):
+    r1, c1 = arr[i]
+    for j in range(N):
+        if i == j:  continue
+        r2, c2 = arr[j]
+        if (r1 + r2 <= H and max(c1, c2) <= W) or (max(r1, r2) <= H and c1 + c2 <= W):
+            max_area = max(max_area, r1 * c1 + r2 * c2)
+        if (c1 + r2 <= H and max(r1, c2) <= W) or (max(c1, r2) <= H and r1 + c2 <= W):
+            max_area = max(max_area, r1 * c1 + r2 * c2)
+        if (c1 + c2 <= H and max(r1, r2) <= W) or (max(c1, c2) <= H and r1 + r2 <= W):
+            max_area = max(max_area, r1 * c1 + r2 * c2)
+        if (r1 + c2 <= H and max(c1, r2) <= W) or (max(r1, c2) <= H and c1 + r2 <= W):
+            max_area = max(max_area, r1 * c1 + r2 * c2)
+
+print(max_area)
