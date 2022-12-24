6df02f0ecacd3457492fd485a0b26b6be02f1857
JaeGun.S <99076885+RUNGOAT@users.noreply.github.com>
2022-12-24 21:10:27 +0900

    [Silver I] Title: 안전 영역, Time: 648 ms, Memory: 30692 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,44 +1,39 @@
 import sys
 input = sys.stdin.readline
-from collections import  deque
+
+
+def soaked_city(rain):
+    for i in range(N):
+        for j in range(N):
+            if arr[i][j] <= rain:
+                visited[i][j] = True
 
 
 def bfs(x, y):
-    q = deque([[x, y]])
-    visited[x][y] = 1
-    while q:
-        x, y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+    stack = [(x, y)]
+    visited[x][y] = True
+    while stack:
+        x, y = stack.pop()
+        for nx, ny in [(x+1, y), (x, y+1), (x-1, y), (x, y-1)]:
             if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny]:
-                q.append([nx, ny])
-                visited[nx][ny] = 1
+                visited[nx][ny] = True
+                stack.append((nx, ny))
 
 
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
+
 ans = 0
-for k in range(101):
-    visited = [[0] * N for _ in range(N)]
-    tmp = []
+for rain in range(101):
+    visited = [[False] * N for _ in range(N)]
+    soaked_city(rain)
+
     cnt = 0
     for i in range(N):
         for j in range(N):
-            if arr[i][j] > k:
-                tmp.append([i, j])
-            else:
-                visited[i][j] = 1
-
-    for x, y in tmp:
-        if not visited[x][y]:
-            bfs(x, y)
-            cnt += 1
-    if cnt == 0:
-        print(ans)
-        exit(0)
+            if not visited[i][j]:
+                bfs(i, j)
+                cnt += 1
 
     ans = max(ans, cnt)
 
