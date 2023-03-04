60bfd0adaed425fffe3888bb055c261a9c06cb77
Jaegun.S <sjk1052005@gmail.com>
2023-03-04 17:44:10 +0900

    [level 3] Title: 양과 늑대, Time: 0.52 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.py
@@ -1,25 +1,23 @@
-def dfs(sheep, wolf, edges, visited, info):
-    global answer
-    if sheep > wolf:
-        answer.append(sheep)
-    else:
-        return
-    
-    for p, c in edges:
-        if visited[p] and not visited[c]:
-            visited[c] = True
-            if info[c] == 0:
-                dfs(sheep + 1, wolf, edges, visited, info)
-            else:
-                dfs(sheep, wolf + 1, edges, visited, info)
-            visited[c] = False
-
-
 def solution(info, edges):
-    global answer
     visited = [False] * len(info)
     answer = []
+    
+    def dfs(sheep, wolf):
+        if sheep > wolf:
+            answer.append(sheep)
+        else:
+            return
+
+        for p, c in edges:
+            if visited[p] and not visited[c]:
+                visited[c] = True
+                if info[c] == 0:
+                    dfs(sheep + 1, wolf)
+                else:
+                    dfs(sheep, wolf + 1)
+                visited[c] = False
+    
     visited[0] = True
-    dfs(1, 0, edges, visited, info)
+    dfs(1, 0)
     
     return max(answer)
