3b6b84fc619c559024023c4daea03756c34fca72
Jaegun.S <sjk1052005@gmail.com>
2023-03-25 22:57:53 +0900

    [Bronze V] Title: 나머지, Time: 76 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -1,19 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
+
+	static int A, B, C;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int A = Integer.parseInt(st.nextToken());
-		int B = Integer.parseInt(st.nextToken());
-		int C = Integer.parseInt(st.nextToken());
-		
+		String[] i = br.readLine().split(" ");
+		A = Integer.parseInt(i[0]);
+		B = Integer.parseInt(i[1]);
+		C = Integer.parseInt(i[2]);
+
 		System.out.println((A + B) % C);
-		System.out.println(((A % C) + (B%C))%C);
+		System.out.println(((A % C) + (B % C)) % C);
 		System.out.println((A * B) % C);
-		System.out.println(((A % C) * (B%C))%C);
+		System.out.println(((A % C) * (B % C)) % C);
 	}
 }
