11a9d37c3b32d267677838a4e9c2bcb5066dcb55
Jaegun.S <sjk1052005@gmail.com>
2023-03-26 00:04:39 +0900

    [Bronze V] Title: 개수 세기, Time: 80 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,17 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
 
 public class Main {
+	
+	public static int count(String[] arr, String v) {
+		int cnt = 0;
+		for (String ar : arr) {
+			if (ar.equals(v)) cnt++;
+		}
+		return cnt;
+	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		int N = Integer.parseInt(br.readLine());
 		String[] arr = br.readLine().split(" ");
 		String v = br.readLine();
-		System.out.println(Collections.frequency(Arrays.asList(arr), v));
+		System.out.println(count(arr, v));
 		br.close();
 	}
 
