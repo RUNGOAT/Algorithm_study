52c5aa33253e0d2058c8deda592717da8bb7fc85
Jaegun.S <sjk1052005@gmail.com>
2023-03-27 23:38:50 +0900

    [Gold IV] Title: 우체국, Time: 476 ms, Memory: 46132 KB -BaekjoonHub

--- 백준/Gold/2141. 우체국/우체국.java
+++ 백준/Gold/2141. 우체국/우체국.java
@@ -2,22 +2,23 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static long X;
-	static long A;
 	static StringTokenizer st;
 
-	private static class Node {
-		long x;
-		long a;
+	private static class Node implements Comparable<Node> {
+		int x, a;
 
-		public Node(long x1, long a1) {
+		public Node(int x1, int a1) {
 			this.x = x1;
 			this.a = a1;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			return this.x - o.x;
 		}
 	}
 
@@ -25,36 +26,26 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		Node[] arr = new Node[N];
-
+		long total = 0;
 		long sum = 0;
 
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			X = Long.parseLong(st.nextToken());
-			A = Long.parseLong(st.nextToken());
-			arr[i] = new Node(X, A);
-			sum += A;
+			int x = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken());
+			arr[i] = new Node(x, a);
+			total += a;
 		}
 
-		Arrays.sort(arr, new Comparator<Node>() {
+		Arrays.sort(arr);
 
-			@Override
-			public int compare(Node o1, Node o2) {
-				if (o1.x == o2.x)
-					return (int) (o1.a - o2.a);
-				return (int) (o1.x - o2.x);
-			}
-
-		});
-
-		long ssum = 0;
 		for (int i = 0; i < N; i++) {
-			ssum += arr[i].a;
-			if (ssum >= (sum + 1) / 2) {
+			sum += arr[i].a;
+			if (sum >= (total + 1) / 2) {
 				System.out.println(arr[i].x);
 				break;
 			}
 		}
-
+		br.close();
 	}
 }
