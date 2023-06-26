5ccd24997807c6b19d428366c00027a07c51ef53
Jaegun.S <sjk1052005@gmail.com>
2023-06-27 02:01:56 +0900

    [Silver I] Title: 경로 찾기, Time: 208 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            if arr[i][k] == 1 and arr[k][j] == 1:
+                arr[i][j] = 1
+
+for i in range(N):
+    print(" ".join(map(str, arr[i])))
