65c0b37897cf9d1896b10019bfb35c9746a3a3a5
Jaegun.S <sjk1052005@gmail.com>
2023-07-07 00:10:57 +0900

    [Silver II] Title: 촌수계산, Time: 120 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -2,57 +2,52 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static List<Integer>[] adjList;
-    static boolean[] visited;
-    static int[] depth;
+	
+	static int N;
+	static List<Integer>[] adjL;
+	static int Y;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int X = Integer.parseInt(st.nextToken());
-        int Y = Integer.parseInt(st.nextToken());
-
-        adjList = new ArrayList[N+1];
-        for(int i=0; i<=N; i++){
-            adjList[i] = new ArrayList<>();
-        }
-
-        visited = new boolean[N+1];
-        depth = new int[N+1];
-
-        int M = Integer.parseInt(br.readLine());
-        for(int i=0; i<M; i++){
-            st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            adjList[x].add(y);
-            adjList[y].add(x);
-        }
-
-        bfs(X);
-
-        if(visited[Y]){
-            System.out.println(depth[Y]);
-        } else {
-            System.out.println("-1");
-        }
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	N = Integer.parseInt(br.readLine());
+    	StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    	int X = Integer.parseInt(st.nextToken());
+    	Y = Integer.parseInt(st.nextToken());
+    	int M = Integer.parseInt(br.readLine());
+    	
+    	adjL = new ArrayList[N+1];
+    	for (int i = 0; i <= N; i++) {
+    		adjL[i] = new ArrayList<>();
+    	}
+    	
+    	for (int i = 0; i < M; i++) {
+    		st = new StringTokenizer(br.readLine());
+    		int x = Integer.parseInt(st.nextToken());
+    		int y = Integer.parseInt(st.nextToken());
+    		
+    		adjL[x].add(y);
+    		adjL[y].add(x);
+    	}
+    	
+    	System.out.println(bfs(X, Y));
     }
-
-    public static void bfs(int x) {
-        Queue<Integer> queue = new LinkedList<>();
-        visited[x] = true;
-        queue.add(x);
-
-        while(!queue.isEmpty()){
-            int curX = queue.poll();
-            for(int i : adjList[curX]){
-                if(!visited[i]){
-                    visited[i] = true;
-                    depth[i] = depth[curX] + 1;
-                    queue.add(i);
-                }
-            }
-        }
+    
+    static int bfs(int x, int y) {
+    	Queue<Integer> q = new LinkedList<>();
+    	q.add(x);
+    	int[] visited = new int[N+1];
+    	visited[x] = 1;
+    	while (!q.isEmpty()) {
+    		int curX = q.poll();
+    		for (int next : adjL[curX]) {
+    			if (visited[next] != 0)	continue;
+				if (next == Y) {
+					return visited[curX];
+				}
+				q.add(next);
+				visited[next] = visited[curX] + 1;
+    		}
+    	}
+    	return -1;
     }
 }
