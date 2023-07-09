21b55d681215c464265aefe9ff9d1b0b0c9867cc
Jaegun.S <sjk1052005@gmail.com>
2023-07-09 14:39:56 +0900

    [Gold IV] Title: 같은 수로 만들기, Time: 132 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Gold/2374. 같은 수로 만들기/같은 수로 만들기.java
+++ 백준/Gold/2374. 같은 수로 만들기/같은 수로 만들기.java
@@ -1,59 +1,65 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-	static int N;
-	static long answer;
+    static int N;
+    static long answer;
+    static int[] arr;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		divide(arr, arr[getMaxIndex(arr)]);
-		System.out.println(answer);
-	}
-	
-	static void divide(int[] arr, int max) {
-		if (arr.length == 0) {
-			return;
-		}
-		if (arr.length == 1 || sameCheck(arr)) {
-			answer += max - arr[0];
-			return;
-		}
-		int maxIdx = getMaxIndex(arr);
-		divide(Arrays.copyOfRange(arr, 0, maxIdx), arr[maxIdx]);
-		
-		if (maxIdx+1 < arr.length) {
-			divide(Arrays.copyOfRange(arr, maxIdx+1, arr.length), arr[maxIdx]);
-		}
-		
-		answer += max - arr[maxIdx];
-	}
-	
-	static int getMaxIndex(int[] arr) {
-		int max = 0;
-		int index = 0;
-		for (int i = 0; i < arr.length; i++) {
-			if (max < arr[i]) {
-				max = arr[i];
-				index = i;
-			}
-		}
-		return index;
-	}
-	
-	static boolean sameCheck(int[] arr) {
-		int first = arr[0];
-		for (int i = 1; i < arr.length; i++) {
-			if (first != arr[i]) {
-				return false;
-			}
-		}
-		return true;
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        calculateDifference(0, N, findMaxValue(0, N));
+        System.out.println(answer);
+    }
+
+    static void calculateDifference(int start, int end, int max) {
+        if (end <= start) {
+            return;
+        }
+        if (end - start == 1 || checkIfAllSame(start, end)) {
+            answer += max - arr[start];
+            return;
+        }
+        int maxIdx = findMaxIndex(start, end);
+        calculateDifference(start, maxIdx, arr[maxIdx]);
+        calculateDifference(maxIdx + 1, end, arr[maxIdx]);
+        answer += max - arr[maxIdx];
+    }
+
+    static int findMaxIndex(int start, int end) {
+        int max = Integer.MIN_VALUE;
+        int index = -1;
+        for (int i = start; i < end; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    static int findMaxValue(int start, int end) {
+        int max = Integer.MIN_VALUE;
+        for (int i = start; i < end; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    static boolean checkIfAllSame(int start, int end) {
+        int first = arr[start];
+        for (int i = start + 1; i < end; i++) {
+            if (first != arr[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
