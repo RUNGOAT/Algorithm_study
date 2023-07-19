d535946a87230a966115b19b45bc52ef27a59b06
Jaegun.S <sjk1052005@gmail.com>
2023-07-19 20:44:56 +0900

    [Gold III] Title: 나무 재테크, Time: 936 ms, Memory: 304588 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.java
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.java
@@ -1,15 +1,29 @@
 import java.io.*;
 import java.util.*;
 
+class Tree implements Comparable<Tree> {
+	int x, y, age;
+	Tree (int x, int y, int age) {
+		this.x = x;
+		this.y = y;
+		this.age = age;
+	}
+	
+	@Override
+	public int compareTo(Tree o) {
+		return Integer.compare(this.age, o.age);
+	}
+	
+}
+
 public class Main {
 	
 	static int N, M, K;
 	static int[][] A;
 	static int[][] map;
-	static List<Integer>[][] trees;
-	static int[][] diedTree;
 	static int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
 	static int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+	static Deque<Tree> treeList;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,93 +33,78 @@ public class Main {
 		K = Integer.parseInt(st.nextToken());
 		
 		map = new int[N][N];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				map[i][j] = 5;
-			}
-		}
-		
 		A = new int[N][N];
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			for (int j = 0; j < N; j++) {
 				A[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-
-		trees = new ArrayList[N][N];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				trees[i][j] = new ArrayList<Integer>();
+				map[i][j] = 5;
 			}
 		}
 		
+		treeList = new LinkedList<>();
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			int x = Integer.parseInt(st.nextToken()) - 1;
 			int y = Integer.parseInt(st.nextToken()) - 1;
 			int z = Integer.parseInt(st.nextToken());
 			
-			trees[x][y].add(z);
+			treeList.add(new Tree(x, y, z));
 		}
-		
-		diedTree = new int[N][N];
 		
 		for (int k = 0; k < K; k++) {
-			spendFirstHalf();
-			spendSecondHalf();
+			spend();
 		}
 		
-		int count = 0;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				count += trees[i][j].size();
-			}
-		}
-		System.out.println(count);
+		System.out.println(treeList.size());
 	}
 	
-	// 봄 - 자신의 나이만큼 양분을 먹음(한 칸에 여러 개면 어린 나무부터, 나이만큼 양분을 못 먹으면 죽음) -> 나이가 1 증가
-	static void spendFirstHalf() {
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				List<Integer> treeList = trees[i][j];
-				Collections.sort(treeList);
-				List<Integer> newTree = new ArrayList<>();
-				for (int age : treeList) {
-					if (age <= map[i][j]) {
-						map[i][j] -= age;
-						newTree.add(++age);
-					} else {
-						// 여름 - 봄에 죽은 나무가 양분으로 변함 (나이를 2로 나눈 값이 해당 칸에 추가됨)
-						diedTree[i][j] += age / 2;
-					}
-				}
-				map[i][j] += diedTree[i][j];
-				diedTree[i][j] = 0;
-				trees[i][j] = newTree;
+	static void spend() {
+		Queue<Tree> diedTree = new LinkedList<>();
+		
+		// 봄
+		for (int i = 0; i < treeList.size();) {
+			Tree cur = treeList.pollFirst();
+			if (map[cur.x][cur.y] >= cur.age) {
+				map[cur.x][cur.y] -= cur.age;
+				cur.age++;
+				i++;
+				treeList.addLast(cur);
+			} else {
+				diedTree.add(cur);
 			}
 		}
-	}
-	
-	// 가을 - 번식 (나이가 5의 배수인 나무, 인접한 8개의 칸, map 벗어나면 안됨)
-	static void spendSecondHalf() {
+		
+		// 여름
+		for (Tree tree : diedTree) {
+			map[tree.x][tree.y] += tree.age / 2;
+		}
+		
+		// 가을
+		Stack<Tree> possibleList = new Stack<>();
+		for (Tree tree : treeList) {
+			if (tree.age % 5 == 0) {
+				possibleList.add(tree);
+			}
+		}
+		while (!possibleList.isEmpty()) {
+			Tree tree = possibleList.pop();
+			
+			for (int d = 0; d < 8; d++) {
+				int nx = tree.x + dx[d];
+				int ny = tree.y + dy[d];
+				if (0 > nx || N <= nx || 0 > ny || N <= ny)
+					continue;
+				treeList.addFirst(new Tree(nx, ny, 1));
+			}
+		}
+		
+		// 겨울
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				for (int age : trees[i][j]) {
-					if (age % 5 == 0) {
-						for (int d = 0; d < 8; d++) {
-							int nx = i + dx[d];
-							int ny = j + dy[d];
-							if (0 > nx || N <= nx || 0 > ny || N <= ny)
-								continue;
-							trees[nx][ny].add(1);
-						}
-					}
-				}
-				// 겨울 - 양분을 추가 A[r][c]의 값이 추가됨
 				map[i][j] += A[i][j];
 			}
 		}
 	}
+	
 }
