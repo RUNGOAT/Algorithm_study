ab657694f0dc5e4bd388549a245ee74597b679fc
Jaegun.S <sjk1052005@gmail.com>
2023-08-13 22:05:00 +0900

    [Gold V] Title: 꿀 따기, Time: 348 ms, Memory: 25016 KB -BaekjoonHub

--- 백준/Gold/21758. 꿀 따기/꿀 따기.java
+++ 백준/Gold/21758. 꿀 따기/꿀 따기.java
@@ -7,13 +7,15 @@ import java.util.StringTokenizer;
 
 public class Main
 {
+	static int N;
 	static short[] arr;
 	
     public static void main(String args[]) throws IOException
     {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    	int N = Integer.parseInt(br.readLine());
+    	
+    	N = Integer.parseInt(br.readLine());
     	StringTokenizer st = new StringTokenizer(br.readLine(), " ");
     	
     	arr = new short[N];
@@ -25,26 +27,16 @@ public class Main
     	
     	// 벌통 오른 끝
     	int first = sum(1, N);
-    	int second = first;	// 반복문에서 시작값을 뺄 예정
-    	for (int i = 1; i < N-1; i++) {
-    		int secondStart = arr[i];
-    		second -= secondStart;
-    		answer = Math.max(answer, first + second - secondStart);
-    	}
+    	answer = Math.max(answer, findMaxHoney(1, N-1, 1, first));
     	
     	// 벌통 왼 끝
     	first += arr[0] - arr[N-1];
-    	second = first;
-    	for (int i = N-2; i > 0; i--) {
-    		int secondStart = arr[i];
-    		second -= secondStart;
-    		answer = Math.max(answer, first + second - secondStart);
-    	}
+    	answer = Math.max(answer, findMaxHoney(N-2, 0, -1, first));
     	
     	// 벌통 중간쯤
     	// 벌통 = i
     	// 벌통은 벌 두 마리가 방문한다.
-    	int ggul = sum(1, N-1);
+    	int ggul = first - arr[0];
     	for (int i = 1; i < N-1; i++) {
     		answer = Math.max(answer, ggul + arr[i]);
     	}
@@ -54,6 +46,19 @@ public class Main
     	bw.close();
     	br.close();
 	}
+    
+    static int findMaxHoney(int start, int end, int value, int first) {
+    	int answer = 0;
+    	
+    	int second = first;	// 반복문에서 시작값을 뺄 예정
+    	for (int i = start; i != end; i += value) {
+    		int secondStart = arr[i];
+    		second -= secondStart;
+    		answer = Math.max(answer, first + second - secondStart);
+    	}
+    	
+    	return answer;
+    }
     
     static int sum(int start, int end) {
     	int sum = 0;
