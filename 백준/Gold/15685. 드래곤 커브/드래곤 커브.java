4756cb404e7f9c91d2fd25f3ad213ecaf8480e16
Jaegun.S <sjk1052005@gmail.com>
2023-09-06 01:03:50 +0900

    [Gold IV] Title: 드래곤 커브, Time: 148 ms, Memory: 15884 KB -BaekjoonHub

--- 백준/Gold/15685. 드래곤 커브/드래곤 커브.java
+++ 백준/Gold/15685. 드래곤 커브/드래곤 커브.java
@@ -8,64 +8,67 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int N;
-	static int[] dx = {0, -1, 0, 1};
-	static int[] dy = {1, 0, -1, 0};
-	
-	public static void main(String args[]) throws IOException {
+
+	private static final int[] DX = { 0, -1, 0, 1 };
+	private static final int[] DY = { 1, 0, -1, 0 };
+	private static final int GRID_SIZE = 101;
+
+	static boolean[][] visited;
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
-		
-		boolean[][] visited = new boolean[101][101];
-		
-		N = Integer.parseInt(br.readLine());
+
+		int N = Integer.parseInt(br.readLine());
+		visited = new boolean[GRID_SIZE][GRID_SIZE];
+
 		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 			int y = Integer.parseInt(st.nextToken());
 			int x = Integer.parseInt(st.nextToken());
 			int d = Integer.parseInt(st.nextToken());
 			int g = Integer.parseInt(st.nextToken());
-			
-			visited[x][y] = true;
-			
-			int nx = x + dx[d];
-			int ny = y + dy[d];
-			visited[nx][ny] = true;
-			x = nx;
-			y = ny;
-			
-			List<Integer> dList = new ArrayList<>();
-			dList.add(d);
-			for (int j = 0; j < g; j++) {
-				List<Integer> newDList = new ArrayList<>();
-				for (int k = dList.size() - 1; k >= 0; k--) {
-					d = (dList.get(k) + 1) % 4;
-					nx = x + dx[d];
-					ny = y + dy[d];
-					visited[nx][ny] = true;
-					x = nx;
-					y = ny;
-					newDList.add(d);
-				}
-				dList.addAll(newDList);
-			}
-			
+
+			makeDragonCurve(x, y, d, g);
 		}
-		
-		bw.write(check(visited) + "\n");
+
+		bw.write(checkAnswer() + "\n");
 		bw.flush();
 		bw.close();
 		br.close();
 	}
-	
-	static int check(boolean[][] visited) {
+
+	private static void makeDragonCurve(int x, int y, int d, int g) {
+		visited[x][y] = true;
+		int nx = x + DX[d];
+		int ny = y + DY[d];
+		visited[nx][ny] = true;
+		x = nx;
+		y = ny;
+
+		List<Integer> directions = new ArrayList<>();
+		directions.add(d);
+
+		for (int i = 0; i < g; i++) {
+			List<Integer> newDirections = new ArrayList<>();
+			for (int idx = directions.size() - 1; idx >= 0; idx--) {
+				d = (directions.get(idx) + 1) % 4;
+				nx = x + DX[d];
+				ny = y + DY[d];
+				visited[nx][ny] = true;
+				x = nx;
+				y = ny;
+				newDirections.add(d);
+			}
+			directions.addAll(newDirections);
+		}
+	}
+
+	private static int checkAnswer() {
 		int answer = 0;
-		for (int i = 0; i < 100; i++) {
-			for (int j = 0; j < 100; j++) {
-				if (visited[i][j] && visited[i][j+1]
-						&& visited[i+1][j] && visited[i+1][j+1]) {
+		for (int i = 0; i < GRID_SIZE - 1; i++) {
+			for (int j = 0; j < GRID_SIZE - 1; j++) {
+				if (visited[i][j] && visited[i][j + 1] && visited[i + 1][j] && visited[i + 1][j + 1]) {
 					answer++;
 				}
 			}
